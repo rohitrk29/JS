265e838bb57e9c5c84d3d5f398e546f8e896abe0
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-03-04 21:13:56 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(2 + 3)  // we are using nodejs, not browser
+
+console.log(2 + 3);
+
+console.log("Rohit")
+
+let name = "rohit"
+let age = 22
+let isloggedIn = true
+
+// DataTypes
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol => uniqueness
+
+// Object
+
+console.log(typeof "rohit" );  //string
+console.log(typeof age);  //number
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
