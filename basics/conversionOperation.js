541bea8a9ba8574c40393e9ba1c658afcc31af91
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-03-05 13:25:48 +0000

    oprations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let Name= "Rohit"
 
 console.log(typeof Name);   // Type = string
@@ -25,3 +26,42 @@ let someNumber = 33
 let stringNumber =String(someNumber)
 console.log(stringNumber);     // o/p => 33 as string
 console.log(typeof stringNumber);
+*/
+
+// ****************Operations**********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 4);
+console.log(2 / 3);
+console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " rohit"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);  //12
+console.log(1 + "2");  //12 
+console.log("1" + 2 + 2);  //122
+console.log(1 + 2 + "2" );  //32
+
+console.log((3 + 4) * 5 % 3);
+
+// (+) use for conversion
+console.log(+true);  //1
+console.log(+"");   //0  empty sting reps 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 3
+
+let gameCounter = 100
+// ++gameCounter;          //prefix
+gameCounter++;          //postfix
+console.log(gameCounter);
