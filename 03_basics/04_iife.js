8dee94dd74c07914441255012ddb80a61379e3f6
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-03-07 14:56:47 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,21 @@
+// ImmediatEly Invoked Function Expression (IIFE)
+
+// ( () {})()
+
+// (function chai(){
+//     console.log(`DB CONN`);
+// })
+// chai()
+
+
+
+(function chai(){
+    console.log(`DB CONNECTED`)
+})();   // ; terminator is use to terminate 1st iife and execute another iife. 
+
+
+//   ( () => {} )()      //with arrow function 
+
+((name) => {
+    console.log(`DB CONNECTED AGAIN ${name}`);
+})('Rohit')
