452ce56311d3170a13a954df30d9ee94f89b42ec
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-03-07 06:15:10 +0000

    .

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+// Objects can be declare by two types : 
+// 1> Literals  2> Constructor
+
+// Singleton by only using constructor not literals 
+
+
+// object literals
+
+const JsUser = {
+    name : "Rohit",
+    age: 22,
+    location: "Bangalore",
+    email: "rohit@gmail.com",
+    isLoggesIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
