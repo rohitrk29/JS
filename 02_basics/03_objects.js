0ab95b96cade4bd2df11292da1d0ddc76866188a
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-03-07 07:48:05 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -4,10 +4,16 @@
 // Singleton by only using constructor not literals 
 
 
+// Object.create  // by constructor
+
+
 // object literals
+const mysymb = Symbol("key1") //Symbol
 
 const JsUser = {
     name : "Rohit",
+    "full name" : "Rohit kumar",
+    [mysymb] : "mykey1",    // defining Symbol using sqr bracket
     age: 22,
     location: "Bangalore",
     email: "rohit@gmail.com",
@@ -15,3 +21,30 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
+//access object
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+// console.log(JsUser.full name); //you can't access the value of full name by using dot(.) operator.
+console.log(JsUser["full name"]);  // to access the full name value, you have to use Square bracket[].
+
+//symbol access
+console.log(JsUser[mysymb]);
+
+JsUser.email = "rohitrk@gmail.com"
+// Object.freeze(JsUser)   //freeze is use restrict the change. it won't allow us to change the value.  
+JsUser.email = "rohitkumar@gmail.com"
+console.log(JsUser.email);
+
+
+
+JsUser.greeting = function() {
+    console.log("hello JS user");
+}
+
+JsUser.greeting2 = function() {
+    console.log(`hello JS user ${this.name}`);       //this keyword
+    console.log(`hello JS user ${this["full name"]}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greeting2());
