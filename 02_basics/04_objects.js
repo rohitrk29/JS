6908bdf6fd412c55938892ea6d02fbdf7b7d3100
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-03-07 11:03:03 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,64 @@
+//Singleton object creation
+    // const instaUser =new Object()
+
+
+//Non-Singleton object creation
+const instaUser = {}
+instaUser.id = "123abc"
+instaUser.name = "Rohit"
+instaUser.isLoggedIn = false
+
+// console.log(instaUser);
+
+
+const regularUser = {
+    email: "rohit@gmal.com",
+    fullname: {
+        userfullname: {
+            firstname: "Rohit",
+            lastname: "Kumar"
+        }
+    }
+}
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname.lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({}, obj1, obj2)  // {} this empty object is target and other are source
+
+const obj3 = {...obj1, ...obj2} //using speard method()
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "rohit@gmail.com"
+    },
+    {
+        id: 1,
+        email: "rohit@gmail.com"
+    },
+    {
+        id: 1,
+        email: "rohit@gmail.com"
+    }
+]
+
+users[1].email
+/*console.log(users);
+
+console.log(instaUser);
+//access the keys and their values in the objects
+console.log(Object.keys(instaUser));
+console.log(Object.values(instaUser));
+console.log(Object.entries(instaUser));
+
+console.log(instaUser.hasOwnProperty('isLoggedIn')); //it will check the value that is present or not. and return in boolean true or false.
+*/
+
+
