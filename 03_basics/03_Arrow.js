ea48c5c02290cd4e8dcd8bb87edaaecd505b89c7
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-03-08 21:47:45 +0000

    .

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,10 @@
+// This keywords =>tells us about the current content
+
+const user = {
+    username: "Rohit",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to website`);
+    }
+}
