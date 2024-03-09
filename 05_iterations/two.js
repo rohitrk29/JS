6e34bd10578a774695c49ff44971ee0492f99814
rohitrk29 <149864470+rohitrk29@users.noreply.github.com>
2024-03-09 14:47:30 +0000

    .

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+//while 
+
+/*
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+*/
+
+/*
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+
+while(arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+*/
+
+let score = 0   // let score = 11 then it will print 11 bcoz do while loop atleast execute once
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
